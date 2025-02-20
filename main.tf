e1e12264d22c04ddf97f5039ad5d0c307345b654
jimmyto-09 <jimmy.acastilloa@gmail.com>
2025-02-20 14:28:18 +0100

    ServidorNginx

---
+++ main.tf
@@ -0,0 +1,58 @@
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name = "nginx"
+    labels = {
+      app = "nginx"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "nginx"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "nginx"
+        }
+      }
+
+      spec {
+        container {
+          image = "nginx:latest"
+          name  = "nginx"
+
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name = "nginx"
+  }
+
+  spec {
+    selector = {
+      app = "nginx"
+    }
+
+    port {
+      port        = 80
+      target_port = 80
+      node_port   = 31025  # Usa el mismo que Kubernetes asignó
+    }
+
+    type = "NodePort"
+  }
+}
+
